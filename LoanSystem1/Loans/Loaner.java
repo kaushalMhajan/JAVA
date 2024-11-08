deb58cca6b8112900af5352f21aa8eb8e7e226ce
Kaushal Atmaram Mahajan <142385433+kaushalMhajan@users.noreply.github.com>
2024-11-08 19:16:05 +0530

    Update Loaner.java

--- LoanSystem1/Loans/Loaner.java
+++ LoanSystem1/Loans/Loaner.java
@@ -14,36 +14,6 @@ public class Loaner {
             sc.setPrinciple(pr);
             sc.setPeriod(p);
             return sc;
-        } 
-    
-        public static double getEmI(Loan l) throws Throwable{
-            if(l instanceof HomeLoan){
-                return l.getPrinciple() * (1 + l.getRate() * l.getPeriod()/100) / (12 * l.getPeriod());
-            }else if(l instanceof PersonalLoan){
-                return l.getPrinciple() * (1 + l.getRate() * l.getPeriod()/100) / (12 * l.getPeriod());
-            }else{
-                throw new Exception();
-            }
-    }
+        }
 
-}
-
-// public static double GetDiscountedEmi(Loan hl){
-//     if(hl is HomeLoan){
-//         IDisscountable h = hl as IDisscountable;
-
-//         double d = h.GetDiscount(); 
-//         return GetEmI(hl)-(GetEmI(hl)* d);
-//     }
-//     return 0;
-// }
-
-// public static double GetTaxedEmi(Loan hl){
-//     if(hl is PersonalLoan){
-//         ITaxable h = hl as ITaxable;
-
-//         double d = h.GetTax(); 
-//         return GetEmI(hl)+(GetEmI(hl)* d);
-//     }
-//     return 0;
-// }   
+} 
